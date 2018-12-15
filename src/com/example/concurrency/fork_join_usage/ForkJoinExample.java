72cd4d6b8262dbf5bbd6292903108caa08a78436
chintoz <jacintomena@gmail.com>
2018-12-15 01:19:44 +0100

    Added example related with Fork/Join framework.

--- src/com/example/concurrency/fork_join_usage/ForkJoinExample.java
+++ src/com/example/concurrency/fork_join_usage/ForkJoinExample.java
@@ -1,8 +1,40 @@
 package com.example.concurrency.fork_join_usage;
 
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * In this example we show how to calculate Fibonacci number using Fork/Join framework.
+ */
 public class ForkJoinExample {
 
     public static void main(String[] args) {
-        // TODO: Complete it!!
+        FiboTask fibonacci = new FiboTask(40);
+        ForkJoinPool pool = new ForkJoinPool();
+        int result = pool.invoke(fibonacci);
+        System.out.println(result);
+    }
+
+    /**
+     * RecursiveTask created for Fork/Join Framework. This kind of class return a value
+     */
+    public static class FiboTask extends RecursiveTask<Integer> {
+
+        final int number;
+
+        FiboTask(int number) {
+            this.number = number;
+        }
+
+        @Override
+        protected Integer compute() {
+            if (number <= 1) {
+                return number;
+            }
+            FiboTask task1 = new FiboTask(number - 1);
+            task1.fork();
+            FiboTask task2 = new FiboTask(number - 2);
+            return task2.compute() + task1.join();
+        }
     }
 }
--- src/com/example/concurrency/fork_join_usage/README.md
+++ src/com/example/concurrency/fork_join_usage/README.md
@@ -1,3 +1,8 @@
 # How can we use the default thread pool generated in Java?
 
-Complete it!!
+Java provides us by default a Thread Pool called ForkJoinPool pool. As we saw in the example for parallel streams, is the one used to launch parallel streams.
+
+We can take advantage of this pool, but the size of this pool is limited by this formula: nº CPUs - 1.
+
+Furthermore, Java offers us a framework 
+
